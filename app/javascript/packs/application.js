f8b973b798807a71f4c5740fd1ea1b8127d72ca8
max <maxpower@riseup.net>
2021-03-15 21:13:24 -0300

    Boostrap-design-material

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+// Librerías que necesitamos.
+import 'bootstrap-material-design'
+
+// import de scss
+import '../sipepa/application'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
