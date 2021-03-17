2dec3498f849b6ed000b963e033315e027268dff
max <maxpower@riseup.net>
2021-03-17 20:32:43 -0300

    Iconos fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,9 @@ import 'bootstrap'
 // import de scss
 import '../sipepa/application'
 
+// import de Íconos fontawesome
+import "@fortawesome/fontawesome-free/css/all"
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
