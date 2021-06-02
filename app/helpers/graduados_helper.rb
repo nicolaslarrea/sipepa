7a171347d90f6ced74bea1e6ca02f13f198db41b
max <maxpower@riseup.net>
2021-06-02 12:31:10 -0300

    Búsqueda de graduado por documento o nombre

--- app/helpers/graduados_helper.rb
+++ app/helpers/graduados_helper.rb
@@ -1,8 +1,8 @@
 # Funciones auxiliares para las vistas de Graduados.
 module GraduadosHelper
-  def filtro_graduado_busqueda_documentos_tag
-    filtro_busqueda_tag key: 'busqueda_documentos',
-      value: busqueda_documentos_actual,
+  def filtro_graduado_por_documento_o_nombre_tag
+    filtro_busqueda_tag key: 'busqueda',
+      value: busqueda_documento_o_nombre_actual,
       url: graduados_path,
       label: 'Documento'
   end
