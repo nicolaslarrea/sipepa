42a4181f10ace9ddbd1efaa446bfc95745e09f3f
max <maxpower@riseup.net>
2021-06-02 17:33:30 -0300

    Estilos del buscador

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,26 @@
 module ApplicationHelper
   # Tag para búsqueda genérica con ajax.
   def filtro_busqueda_tag(key:, value:, url:, label:, data: {})
-    safe_join(
-      [
-        label_tag(label, nil, class: 'bmd-label-floating'),
-        text_field_tag(
-          key,
-          value,
-          data: { url: url, key: key }.merge(data),
-          class: 'form-control select-filtro text-filtro',
-          autofocus: value.present?
-        )
-      ]
+    content_tag(
+      :div,
+      safe_join(
+        [
+          content_tag(
+            :span,
+            content_tag(:i, nil, class: "fas fa-search"),
+            class: "input-group-text border-0 bg-transparent",
+            id: "search-addon"
+          ),
+          text_field_tag(
+            key,
+            value,
+            data: { url: url, key: key }.merge(data),
+            class: 'form-control rounded select-filtro text-filtro',
+            autofocus: value.present?
+          )
+        ]
+      ),
+      class: "input-group rounded"
     )
   end
 end
